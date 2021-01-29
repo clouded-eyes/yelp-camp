b4a95806d1907426f6949c2643ef9f12f80e0e68
Jia Wei Tan <tanjiawei92@gmail.com>
2021-01-29 17:59:17 +0800

    add maps

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,6 +1,10 @@
 const Campground = require("../models/campground");
 const { cloudinary } = require("../cloudinary");
 
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
@@ -11,13 +15,21 @@ module.exports.renderCreatorForm = (req, res) => {
 };
 
 module.exports.create = async (req, res, next) => {
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.author = req.user._id;
   campground.images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
   }));
   await campground.save();
+  console.log(campground);
   req.flash("success", "Successfully made a new campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
--- seeds/index.js
+++ seeds/index.js
@@ -5,6 +5,12 @@ const { places, descriptors } = require("./seedHelpers");
 // console.log(seedHelpers);
 const Campground = require("../models/campground");
 const Review = require("../models/review");
+
+// const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+// const mapBoxToken =
+//   "pk.eyJ1IjoiY2xvdWRlZC1leWVzIiwiYSI6ImNra2U0aGY3ZTA0eHQycW85Y2VlbGE2bm4ifQ.K-sTimArS0kDKkCfFIKJkg";
+// const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -18,7 +24,7 @@ db.once("open", () => {
   console.log("MONGO DB CONNECTED");
 });
 
-const seedNumber = 50;
+const seedNumber = 300;
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
@@ -26,16 +32,33 @@ const seedDB = async () => {
   await Campground.deleteMany({}).then(
     console.log("yelp-camp > campgrounds collection > items deleted")
   );
+
   for (let i = 0; i < seedNumber; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 30) + 10;
+    const location = `${cities[random1000].city}, ${cities[random1000].state}`;
+
+    // const geoData = await geocoder
+    //   .forwardGeocode({
+    //     query: location,
+    //     limit: 1,
+    //   })
+    //   .send();
+
     const camp = new Campground({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      location: location,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ad inventore distinctio, hic veritatis eaque soluta animi aperiam natus rerum ratione maxime corporis maiores consectetur, sint mollitia dolor expedita non repellendus.",
       price: price,
       author: "600d0b9c17978a2c2cc6a336",
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           _id: "600fff5a685335c4cc8b08d2",
